d0ace90d2b389af99ff0e27d34f8dbb33c1454f0
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2018-09-24 15:26:50 +0900

    Cardデザインを触ってみる

--- app/components/Note.js
+++ app/components/Note.js
@@ -11,55 +11,43 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
+import {
+  Card,
+  ListItem,
+  Button,
+  Icon,
+} from 'react-native-elements'
+
+const users = [
+ {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+ }
+]
 
 export default class Note extends Component {
   render() {
     return (
-      <View key={this.props.keyval} style={styles.note}>
+      <Card containerStyle={{padding: 10}} >
         <Text style={styles.noteText}>
-          {this.props.val.date}
+          {this.props.val.date} {this.props.val.note}
         </Text>
-        <Text style={styles.noteText}>
-          {this.props.val.note}
-        </Text>
-
-        <TouchableOpacity onPress={this.props.deleteMethod}
-          style={styles.noteDelete}>
-          <Text style={styles.noteDeleteText}>
-            D
-          </Text>
-        </TouchableOpacity>
-      </View>
+        <Button onPress={this.props.deleteMethod}
+          icon={{name: 'delete'}}
+          backgroundColor='#03A9F4'
+          buttonStyle={{borderRadius: 20, marginLeft: 30, marginRight: 30, marginBottom: 0}}
+          title='delete me'>
+        </Button>
+      </Card>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  note: {
-    position: 'relative',
-    padding: 20,
-    paddingRight: 100,
-    borderBottomWidth: 2,
-    borderBottomColor: '#ededed',
-  },
   noteText: {
-    paddingLeft: 20,
-    borderLeftWidth: 10,
-    borderLeftColor: '#E91E63',
-  },
-  noteDelete: {
-    position: 'absolute',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2980b9',
-    padding: 10,
-    top: 10,
-    bottom: 10,
-    right: 10
-  },
-  noteDeleteText: {
-    color: 'white'
+    borderBottomWidth: 10,
+    paddingLeft: 25
   }
 });
