6b7d8246aa4890ce4b05aed615a924a1d4e037c9
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2018-09-25 08:18:55 +0900

    入力欄を少し大きく

--- app/components/Main.js
+++ app/components/Main.js
@@ -52,7 +52,7 @@ export default class Main extends Component {
             style={styles.textInput}
             onChangeText={(noteText) => this.setState({noteText})}
             value={this.state.noteText}
-            placeholder=">note"
+            placeholder="> note"
             placeholderTextColor="white"
             underlineColoriOS="transparent">
           </TextInput>
@@ -100,18 +100,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  header: {
-    backgroundColor: '#E91E63',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderBottomWidth: 10,
-    borderBottomColor: '#ddd',
-  },
-  headerText: {
-    color: 'white',
-    fontSize: 18,
-    padding: 26,
-  },
   scrollContainer: {
     flex: 1,
     marginBottom: 100,
@@ -129,6 +117,7 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#252525',
     borderTopWidth: 2,
+    fontSize: 20,
     borderTopColor: '#ededed',
   },
 
@@ -137,7 +126,7 @@ const styles = StyleSheet.create({
     zIndex: 11,
     right: 20,
     bottom: 90,
-    backgroundColor: '#E91E63',
+    backgroundColor: '#ffbb70',
     width: 45,
     height: 45,
     borderRadius: 50,
--- app/components/Note.js
+++ app/components/Note.js
@@ -20,32 +20,15 @@ import {
   Icon,
 } from 'react-native-elements'
 
-const users = [
- {
-    name: 'brynn',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
- }
-]
-
 export default class Note extends Component {
   render() {
     return (
-      <Card containerStyle={{padding: 10}} >
-        <Text style={styles.noteTextDate}>
-          {this.props.val.date}
-        </Text>
-        <Text style={styles.noteTextNote}>
-          {this.props.val.note}
-        </Text>
-        <Button onPress={this.props.deleteMethod}
-          icon={{name: 'delete'}}
-          style={styles.deleteButton}
-          buttonStyle={{ borderRadius: 100 }}
-          backgroundColor='#03A9F4'
-          title='DELETE'
-          >
-        </Button>
-      </Card>
+      <ListItem
+        title={this.props.val.note}
+        subtitle={this.props.val.date}
+        rightIcon={{ name: 'delete' }}
+        onPress={this.props.deleteMethod}
+      />
     );
   }
 }
